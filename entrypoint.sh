21a6fcb08913637ef372c5cb762f31d920789ae6
Oliver Tosky <olivertosky@Olivers-MacBook-Pro.local>
2020-11-08 11:59:23 -0500

    Enable sqlfmt flags as hook args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-FMT_PARAMS="--use-spaces --print-width 45"
+# parse args passed by pre-commit to separate filenames from sqlfmt args
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --print-width)  width="--print-width $2"; shift 2;;
+    --tab-width)    indent="--tab-width $2"; shift 2;;
+    --use-spaces)   spaces="--use-spaces"; shift 1;;
+    --align)        align="--align"; shift 1;;
+    --no-simplify)  no_groups="--no-simplify"; shift 1;;
+    
+    *) FILES="$FILES $1"; shift 1;;
+  esac
+done
 
-for filename in $@;
+# concat sqlfmt flags
+FMT_PARAMS="$width $indent $spaces $align $no_groups"
+
+# pass filenames to sqlfmt
+for filename in $FILES;
 do 
   cat $filename  | cockroach sqlfmt $FMT_PARAMS | sponge $filename
 
